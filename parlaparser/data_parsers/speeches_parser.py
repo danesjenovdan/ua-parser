973a8e84c953929afb3d7d8b3cc7b7604b2c880f
Tomaz Kunst <tomazkunst@gmail.com>
2024-10-04 16:22:13 +0200

    port parladata storage to parladata_base_api

--- parlaparser/data_parsers/speeches_parser.py
+++ parlaparser/data_parsers/speeches_parser.py
@@ -1,6 +1,4 @@
 from parlaparser.data_parsers.base_parser import BaseParser
-from parlaparser import settings
-from enum import Enum
 from datetime import datetime
 from babel.dates import format_date
 
@@ -32,28 +30,29 @@ class SpeechesParser(BaseParser):
         ua_date = format_date(start_time, format='d MMMM YYYY', locale='uk')
         sitting_name = f'ЗАСІДАННЯ, {ua_date}'
 
-        session_id = self.data_storage.add_or_get_session({
+        session = self.data_storage.session_storage.get_or_add_object({
             'name': sitting_name,
             'organization': self.data_storage.main_org_id,
             'organizations': [self.data_storage.main_org_id],
-            'start_time': start_time.isoformat()
+            'start_time': start_time.isoformat(),
+            'mandate': self.data_storage.mandate_id,
         })
 
-        if session_id in self.data_storage.sessions_with_speeches:
+        if session.get_speech_count() > 0:
             logging.warning('Speeches of this session was already parsed')
             return
 
         self.speeches = []
         for i, speech in enumerate(data['speeches']):
-            person_id, added_person = data_storage.get_or_add_person(
-                speech['speaker']
-            )
-            data['speeches'][i]['speaker'] = person_id
-            data['speeches'][i]['session'] = session_id
+            person = data_storage.people_storage.get_or_add_object({
+                'name': speech['speaker']
+            })
+            data['speeches'][i]['speaker'] = person.id
+            data['speeches'][i]['session'] = session.id
 
             time = datetime.strptime(speech['time'], '%X').time()
 
             data['speeches'][i]['start_time'] = datetime.combine(start_time.date(), time).isoformat()
 
 
-        self.data_storage.add_speeches(data['speeches'])
+        session.add_speeches(data['speeches'])
